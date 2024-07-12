cf204b2edae8429bddd54e115e0f5c875c9debd9
Martin Brabenec <crazybrab@gmail.com>
2024-07-12 14:26:45 +0200

    v3

--- resources/js/healthycoder/App.jsx
+++ resources/js/healthycoder/App.jsx
@@ -1,50 +1,3 @@
-// import React, { useState } from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Navigation from './components/Navigation';
-// import Sidebar from './components/Sidebar';
-// import Footer from './components/Footer';
-// import Home from './pages/Home';
-// import User from './pages/Users';
-// import Activities from './pages/Activities';
-// import Recipes from './pages/Recipes';
-// import Ingredients from './pages/Ingredients';
-// import Login from './pages/Login';
-// import Register from './pages/Register';
-// import RecipeList from './components/RecipeList';
-// import RecipeDetail from './components/RecipeDetail';
-
-// const App = () => {
-//   const [sidebarVisible, setSidebarVisible] = useState(true);
-
-//   return (
-//     <Router>
-//       <div className="d-flex flex-column min-vh-100">
-//         <Navigation />
-//         <div className="flex-grow-1 d-flex">
-//           <Sidebar visible={sidebarVisible} setVisible={setSidebarVisible} />
-//           <main className={`flex-grow-1 p-3 ${sidebarVisible ? 'main-with-sidebar' : ''}`}>
-//             <Routes>
-//               <Route path="/" element={<Home />} />
-//               <Route path="/users" element={<User />} />
-//               <Route path="/activities" element={<Activities />} />
-//               <Route path="/recipes" element={<Recipes />}>
-//                 <Route index element={<RecipeList />} />
-//                 <Route path=":id" element={<RecipeDetail />} />
-//               </Route>
-//               <Route path="/ingredients" element={<Ingredients />} />
-//               <Route path="/login" element={<Login />} />
-//               <Route path="/register" element={<Register />} />
-//             </Routes>
-//           </main>
-//         </div>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation';
@@ -73,9 +26,8 @@ const App = () => {
               <Route path="/" element={<Home />} />
               <Route path="/users" element={<User />} />
               <Route path="/activities" element={<Activities />} />
-              <Route path="/recipes" element={<Recipes />}>
-                <Route path=":id" element={<RecipeDetail />} />
-              </Route>
+              <Route path="/recipes" element={<Recipes />} />
+              <Route path="/recipes/:id" element={<RecipeDetail />} />
               <Route path="/ingredients" element={<Ingredients />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
--- resources/js/healthycoder/pages/Recipes.jsx
+++ resources/js/healthycoder/pages/Recipes.jsx
@@ -1,26 +1,10 @@
-// import React from 'react';
-// import { Outlet } from 'react-router-dom';
-
-// function Recipes() {
-//   return (
-//     <div>
-//       <h1>Recipes</h1>
-//       <Outlet />
-//     </div>
-//   );
-// }
-
-// export default Recipes;
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link, Outlet, useParams } from 'react-router-dom';
 
 const API_BASE_URL = 'http://www.thehealthycoder.test/api';
 
 function Recipes() {
   const [recipes, setRecipes] = useState([]);
-  const { id } = useParams();
 
   useEffect(() => {
     axios.get(`${API_BASE_URL}/recipes`)
@@ -33,20 +17,17 @@ function Recipes() {
   }, []);
 
   return (
-    <div className="row">
-      <div className="col-md-4">
-        <h2>All Recipes</h2>
-        <ul>
-          {recipes.map(recipe => (
-            <li key={recipe.id}>
-              <Link to={`/recipes/${recipe.id}`}>{recipe.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="col-md-8">
-        <Outlet />
-      </div>
+    <div>
+      <h2>All Recipes</h2>
+      <ul>
+        {recipes.map(recipe => (
+          <li key={recipe.id}>
+            <a href={`/recipes/${recipe.id}`} target="_blank" rel="noopener noreferrer">
+              {recipe.name}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
