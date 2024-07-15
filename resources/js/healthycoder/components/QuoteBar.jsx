603b7f803409d35ac2429815da5f8256ef6fe687
Martin Brabenec <crazybrab@gmail.com>
2024-07-15 15:33:45 +0200

    navbar, quotebar better aligned

---
+++ resources/js/healthycoder/components/QuoteBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../../css/quotebar.scss';
+
+const QuoteBar = () => {
+  return (
+    <div className="quote-bar">
+      <p className="quote-text m-0">Quote of the day: "Your inspirational quote here."</p>
+    </div>
+  );
+};
+
+export default QuoteBar;
--- resources/js/healthycoder/pages/Home.jsx
+++ resources/js/healthycoder/pages/Home.jsx
@@ -1,37 +1,27 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
+import QuoteBar from '../components/QuoteBar';
+import '../../../css/home.scss';
 
 const Home = () => {
   return (
-    <div className="container-fluid">
+    <div className="container-fluid p-0">
       <Sidebar />
+      <QuoteBar />
       <div className="content">
-        <div className="quote-container">
-          <p className="quote-text">Quote of the day: "Your inspirational quote here."</p>
-        </div>
-        <h1 className="mb-4 text-center">Welcome to the Healthy Coder App</h1>
-        <div className="d-flex justify-content-center">
-          <div className="card-deck">
-            <div className="card">
-              <img src="path_to_your_image" className="card-img-top" alt="Description" />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-            <div className="card">
-              <img src="path_to_your_image" className="card-img-top" alt="Description" />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-            <div className="card">
-              <img src="path_to_your_image" className="card-img-top" alt="Description" />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
+        <div className="main-content">
+          <h1 className="mb-4 text-center">Welcome to the Healthy Coder App</h1>
+          <div className="d-flex justify-content-center">
+            <div className="card-deck">
+              {[1, 2, 3].map((index) => (
+                <div key={index} className="card">
+                  <img src="path_to_your_image" className="card-img-top" alt="Description" />
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
